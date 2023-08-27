b081575bbb9578132523710a41b240e7134e2b8f
miyagawadaiki <miyagawa.daiki.18@shizuoka.ac.jp>
2023-08-27 15:59:46 +0900

    ビルドファイル群をhttps使用に修正

--- unity/aws_sample/Assets/Scripts/PublicContents.cs
+++ unity/aws_sample/Assets/Scripts/PublicContents.cs
@@ -1,9 +1,10 @@
 namespace PublicContents {
 
 public static class Constants {
+	public static readonly string PROTOCOL = "https";
 	public static readonly string IP_ADDRESS = "35.77.89.172";
 	public static readonly int PORT = 8000;
-	public static readonly string API_PREFIX = "http://" + IP_ADDRESS + ":" + PORT;
+	public static readonly string API_PREFIX = PROTOCOL + "://" + IP_ADDRESS + ":" + PORT;
 }
 
 
